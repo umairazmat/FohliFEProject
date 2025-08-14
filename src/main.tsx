2c3210b89d836c3cfd51e82fbb84d0134b7bf176
Umair Azmat <devumair260@gmail.com>
2025-08-14 00:05:17 -0700

    graphql issue resolved

--- src/main.tsx
+++ src/main.tsx
@@ -8,14 +8,12 @@ import { AppProviders } from "./app/providers";
 import App from "./app/App";
 
 async function enableMocking() {
-  if (import.meta.env.DEV) {
-    const { worker } = await import("./mocks/browser");
-    await worker.start({
-      onUnhandledRequest: "bypass",
-      serviceWorker: { url: "/mockServiceWorker.js" }, // make sure this matches your public folder
-    });
-    console.log("Mocking enabled", worker);
-  }
+  const { worker } = await import("./mocks/browser");
+  await worker.start({
+    onUnhandledRequest: "bypass",
+    serviceWorker: { url: "/mockServiceWorker.js" }, // must match public folder location
+  });
+  console.log("✅ Mocking enabled");
 }
 
 enableMocking().then(() => {
@@ -27,20 +25,3 @@ enableMocking().then(() => {
     </StrictMode>
   );
 });
-
-// import "./agGridSetup";
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import "./index.css";
-
-// import { AppProviders } from "./app/providers/index.tsx";
-// import "./styles/global.css";
-// import App from "./app/App.tsx";
-
-// createRoot(document.getElementById("root")!).render(
-//   <StrictMode>
-//     <AppProviders>
-//       <App />
-//     </AppProviders>
-//   </StrictMode>
-// );
